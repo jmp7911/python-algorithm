0cdfe2b347aa04ade60bdfc6d820e2b7663db376
JiMan Park <jmp7911@gmail.com>
2024-01-16 00:38:18 +0900

    [level Lv.2] Title: 최솟값 만들기, Time: 0.46 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,11 @@
+def solution(A,B):
+    answer = 0
+
+    # 최대의 값과 최소의 값을 곱하여 결과값이 최소값을 기대
+    A_ = sorted(A, reverse=True)
+    B_ = sorted(B)
+    
+    
+    answer = sum([x[0]*x[1] for x in zip(A_, B_)])
+
+    return answer
