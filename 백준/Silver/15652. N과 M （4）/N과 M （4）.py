4204cdd14b7ed390846ab3f1c6bd20a4b05c46d6
JiMan Park <jmp7911@gmail.com>
2024-03-06 16:04:09 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+
+def backtracking(n, r, result):
+    if not r:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(1, n+1):
+        if not result or result[-1] <= i:
+            result.append(i)
+            backtracking(n, r-1, result)
+            result.pop()
+
+backtracking(N, M, [])
