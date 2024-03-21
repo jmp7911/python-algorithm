59af68c93a25a28eccfc23d5b10db1462d9ebcd7
JiMan Park <jmp7911@gmail.com>
2024-03-21 17:56:03 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -5,14 +5,8 @@ N = int(sys.stdin.readline())
 def factorial(n):
     if n == 0:
         return 1
-    elif n <= 2:
+    elif n == 1:
         return n
-    
-    arr = [0] * (n+1)
-    arr[1] = 1
-    for i in range(2, n+1):
-        arr[i] = arr[i - 1] * i
-    
-    return arr[n]
+    return n * factorial(n - 1)
 
 print(factorial(N))
