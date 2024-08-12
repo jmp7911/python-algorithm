e8e1fa485033405b4b878f58fad355718445696f
JiMan Park <jmp7911@gmail.com>
2024-08-12 16:11:51 +0900

    Time: 56 ms (15.73%), Space: 16.6 MB (49.72%) - LeetHub

---
+++ Leetcode/0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,32 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbols = {
+            1:'I',
+            5:'V',
+            10:'X',
+            50:'L',
+            100:'C',
+            500:'D',
+            1000:'M'
+        }
+        times = 1
+        answer = ''
+        while num > 0:
+            remainer = num % 10
+            num //=  10
+            # print(remainer, times)
+            if remainer == 4:
+                answer = symbols[times] + symbols[times * 5] + answer
+            elif remainer == 9:
+                answer = symbols[times] + symbols[times * 10] + answer
+            elif remainer > 5:
+                answer = symbols[times * 5] + symbols[times] * (remainer - 5) + answer
+            else:
+                if remainer * times in symbols:
+                    answer = symbols[remainer * times] + answer
+                else:
+                    answer = symbols[times] * remainer + answer
+
+            times *= 10
+        return answer
+    
