e2a436903a156ee778bcda94dec0c85dc6a54a27
JiMan Park <jmp7911@gmail.com>
2024-10-04 11:21:08 +0900

    Time: 37 ms (59.47%), Space: 16.6 MB (29.8%) - LeetHub

--- Leetcode/0075-sort-colors/0075-sort-colors.py
+++ Leetcode/0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # insertion sort
+        '''
         i = 1
         n = len(nums)
         while i < n:
@@ -15,4 +17,13 @@ class Solution:
                 i -= 1
             if i == 0:
                 i = 1
-        
+        '''
+        # bubble sort
+        n = len(nums)
+        for _ in range(n):
+            for i in range(1, n):
+                if nums[i-1] > nums[i]:
+                    temp = nums[i-1]
+                    nums[i-1] = nums[i]
+                    nums[i] = temp
+                    
