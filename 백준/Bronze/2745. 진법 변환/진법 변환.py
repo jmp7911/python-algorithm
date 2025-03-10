f9c325de963782ce427b7906054e43ffbd47801f
JiMan Park <jmp7911@gmail.com>
2025-03-10 11:45:45 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,10 +1,3 @@
 N, B = input().split()
 B = int(B)
-result = 0
-for index, i in enumerate(N[::-1]):
-    if i.isalpha():
-        i = ord(i) - 55
-    else:
-        i = int(i)
-    result += i * B ** index
-print(result)
+print(int(N, B))
