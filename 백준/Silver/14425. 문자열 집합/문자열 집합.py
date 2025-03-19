2469aa5c7c2eb4a64899e936835623971f03d231
JiMan Park <jmp7911@gmail.com>
2025-03-19 14:46:26 +0900

    [Silver IV] Title: 문자열 집합, Time: 92 ms, Memory: 43244 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,5 +1,14 @@
 import sys
 N,M=map(int,sys.stdin.readline().split())
+d = dict()
 A=[sys.stdin.readline() for _ in range(N)]
 B=[sys.stdin.readline() for _ in range(M)]
-print(len(list(filter(lambda x: x in A, B))))
+for x in A:
+    d[x] = 1
+cnt = 0
+for x in B:
+    try:
+        cnt += d[x]
+    except:
+        pass
+print(cnt)
