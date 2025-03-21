97916d5b6a0cdfc934f1d7ca1c3fd2c66b92ce48
JiMan Park <jmp7911@gmail.com>
2025-03-21 15:44:13 +0900

    [Silver III] Title: 분수 합, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -0,0 +1,8 @@
+import math, sys
+input = sys.stdin.readline
+A,B=map(int, input().split())
+C,D=map(int, input().split())
+son = A*D+B*C
+mom = B*D
+gcd = math.gcd(son,mom)
+print(son//gcd, mom//gcd)
